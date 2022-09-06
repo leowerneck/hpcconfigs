a02af8b70a0a025a3646ef17dc37dffd959d377f
Leo Werneck <wernecklr@gmail.com>
2022-09-06 12:09:24 -0600

    sawtooth/intel-19.0.5: added intel-19.0.5 configuration for sawtooth

---
+++ sawtooth/intel-19.0.5/modules.sh
@@ -0,0 +1,1 @@
+module load intel/19.0.5-gcc-9.2.0-kl4p intel-mpi/2018.5.288-intel-19.0.5-alnu zlib/1.2.11-intel-19.0.5-p7mu hdf5/1.10.6-intel-19.0.5-m6j7 hwloc/2.1.0-intel-19.0.5-4ncw gsl/2.7_intel19.0.5
