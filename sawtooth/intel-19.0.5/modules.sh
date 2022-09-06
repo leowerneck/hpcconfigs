da3e8f69f156123607589e7f3aa16cc21e558183
Leo Werneck <wernecklr@gmail.com>
2022-09-06 12:10:37 -0600

    sawtooth/intel-19.0.5/modules.sh: updated modules

--- sawtooth/intel-19.0.5/modules.sh
+++ sawtooth/intel-19.0.5/modules.sh
@@ -1,1 +1,3 @@
-module load intel/19.0.5-gcc-9.2.0-kl4p intel-mpi/2018.5.288-intel-19.0.5-alnu zlib/1.2.11-intel-19.0.5-p7mu hdf5/1.10.6-intel-19.0.5-m6j7 hwloc/2.1.0-intel-19.0.5-4ncw gsl/2.7_intel19.0.5
+module purge
+module load intel/19.0.5-gcc-9.2.0-kl4p intel-mpi/2018.5.288-intel-19.0.5-alnu zlib/1.2.11-intel-19.0.5-p7mu hdf5/1.10.6-intel-19.0.5-m6j7 hwloc/2.1.0-intel-19.0.5-4ncw gsl/2.7_intel19.0.5 git/2.25.0-gcc-9.2.0-v4mv
+module list
