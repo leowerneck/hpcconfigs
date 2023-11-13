357460ce47d8bcb08f8463382f80cb7dbdd05d9b
Samuel Cupp <scupp1@my.apsu.edu>
2023-11-13 16:46:16 -0700

    Falcon: updates to scripts

--- etk/falcon/gcc-12.1.0/load_modules.sh
+++ etk/falcon/gcc-12.1.0/load_modules.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Currently Loaded Modules:
-#  1) gcc/12.1.0   2) openmpi/4.1.3   3) hdf5/1.12.2
+#  1) gcc/12.1.0   2) openmpi/4.1.3   3) hdf5/1.12.2   4) cmake/3.24.2
 module purge
-module load gcc/12.1.0 openmpi/4.1.3 hdf5/1.12.2
+module load gcc/12.1.0 openmpi/4.1.3 hdf5/1.12.2 cmake/3.24.2
 module list
--- etk/falcon/gcc-12.1.0/submit.sh
+++ etk/falcon/gcc-12.1.0/submit.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Partition options:
 # Name  Priority MaxTime MaxSubmitPU
 # ----------------------------------
@@ -9,26 +8,35 @@
 # reg      18    7 days      500
 # long      9      --         50
 
-# ntasks-per-node * cpus-per-task should (hopefully) equal 36.
-# Note that the per-node memory is 128GB RAM, so usage above that
-# will require splitting the job across more nodes regardless of cores
+# Falcon has 36 cores/node, so ntasks-per-node*cpus-per-task should equal 36
 
-#SBATCH --job-name=name-me-plz
-#SBATCH --partition=long
-#SBATCH --nodes=2 # 36 processes per node
-#SBATCH --ntasks-per-node=9
-#SBATCH --cpus-per-task=4
-#SBATCH -t 1:00:00 # run time (hh:mm:ss)
-#SBATCH -o test-results-%j # output and error file name (%j expands to jobID)
+# Note 1: per-node memory is 128GB RAM, so usage above that will
+# require splitting the job across more nodes with less processes/node
 
-module load gcc/12.1.0 openmpi/4.1.3 hdf5/1.12.2 fftw/3.3.10
+# Note 2: since falcon has 2 sockets/node, ntasks-per-node should be
+# divisible by 2 so they can be evenly divided across the sockets
 
-export OMP_NUM_THREADS=4
+#SBATCH --job-name=my-job     # Job Name
+#SBATCH -o test-results-%j    # output and error file name (%j expands to jobID)
+#SBATCH --partition=long      # Queue (partition) name
+#SBATCH --nodes=16            # Number of nodes
+#SBATCH -n 96                 # Total number of MPI processes
+#SBATCH --ntasks-per-node=6   # MPI processes per node
+#SBATCH --ntasks-per-socket=3 # MPI processes per socket
+#SBATCH --cpus-per-task=6     # OpenMP processes per node
+#SBATCH -t 336:00:00          # run time (hh:mm:ss)
+
+module purge
+module load gcc/12.1.0 openmpi/4.1.3 hdf5/1.12.2
+module list
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 echo "Preparing run:"
 date
 
-mpirun -n 18 /path/to/my_mpi_executable
+# This automatically sets the ntasks, mapping by socket/threads based on the slurm variables
+mpirun -n $SLURM_NTASKS -x OMP_NUM_THREADS --map-by ppr:$SLURM_NTASKS_PER_SOCKET:socket:pe=$OMP_NUM_THREADS /path/to/my_mpi_executable -reo my.par
 
 echo "Run complete."
 date
