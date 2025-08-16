803fa35d8fb7e77ebe458d5a9726d242debf7163
Leo Werneck <wernecklr@gmail.com>
2025-08-15 17:05:29 -0700

    Ready for merger onto main

---
+++ etk/falcon/apptainer/create_release.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+set -e
+
+# Set the release version
+RELEASE_VERSION=0.1.0
+
+# Set basename
+BASENAME=et_apptainer
+
+# Release name
+RELEASE_NAME=${BASENAME}-v${RELEASE_VERSION}
+
+# Clean up any previous releases
+rm -rf $RELEASE_NAME ${RELEASE_NAME}.tar.gz
+
+# Build the container up to the fourth layer
+make layer4_lib.sif
+
+# Set up the release
+mkdir ${RELEASE_NAME}
+cp layer4_lib.sif ${RELEASE_NAME}/os_ucx_mpi_lib.sif
+cp layer5_et.def ${RELEASE_NAME}/et.def
+sed -i 's/layer4_lib.sif/os_ucx_mpi_lib.sif/' ${RELEASE_NAME}/et.def
+cp bns.th ${RELEASE_NAME}/bns.th
+cat << 'EOF' > ${RELEASE_NAME}/Makefile
+# Makefile for layered Apptainer Einstein Toolkit build
+
+THORNLIST ?= bns.th
+
+LIB_IMAGE := os_ucx_mpi_lib.sif
+ET_INPUT  := et.def
+ET_IMAGE  := et_$(THORNLIST:.th=).sif
+
+# Get full path to the thornlist
+THORNLIST_FULLPATH := $(shell realpath $(THORNLIST))
+
+all: $(ET_IMAGE)
+
+$(ET_IMAGE): $(ET_INPUT) $(LIB_IMAGE)
+	@echo "=== Building Layer 5 --- Einstein Toolkit ==="
+	apptainer build --force                              \
+		--bind $(THORNLIST_FULLPATH):/input_thornlist.th \
+		$(ET_IMAGE) $(ET_INPUT)
+
+clean:
+	@echo "=== Cleaning up Einstein  Toolkit image ==="
+	rm -f $(ET_IMAGE)
+EOF
+
+# Copy the necessary files to the rele
+tar czvf ${RELEASE_NAME}.tar.gz ${RELEASE_NAME}
+
+# Clean up
+rm -rf ${RELEASE_NAME}
